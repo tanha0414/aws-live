195223aeb1628b9235e529f6811154c4dbb109c8
Tan Hi Ann <97738419+tanha0414@users.noreply.github.com>
2022-12-21 17:03:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee2.c9tlv938h2zq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee2"
+custombucket = "tanhiann-employee"
+customregion = "us-east-1"
